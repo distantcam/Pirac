75bafe9c1f450981326225b23a3b8e1710472f46
Cameron MacFarland <distantcam@gmail.com>
2015-03-01 15:45:03 +0800

    Packaging sorted

--- src/Pirac/Properties/AssemblyInfo.cs
+++ src/Pirac/Properties/AssemblyInfo.cs
@@ -4,17 +4,5 @@
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Pirac")]
+[assembly: AssemblyDescription("Yet another MVVM Framework.")]
 [assembly: AssemblyCompany("Cameron MacFarland")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
